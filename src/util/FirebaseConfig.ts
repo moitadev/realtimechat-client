b0a596aa1426843e8558f21f53c3e14bd156169a
Moita <marcos.sales97@gmail.com>
2024-02-09 02:29:20 -0300

    feat(install): add Firebase to project dependencies

---
+++ src/util/FirebaseConfig.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
