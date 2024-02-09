0b8d53a49c47c34a348a55a7f4d55c34692b2a13
Moita <marcos.sales97@gmail.com>
2024-02-09 02:37:41 -0300

    feat(errors): implement localization for error messages

---
+++ src/assets/errors.ts
@@ -0,0 +1,21 @@
+export const errors: Record<string, string> = {
+    'auth/app-deleted': 'O banco de dados não foi localizado.',
+    'auth/api-key-not-valid.-please-pass-a-valid-api-key.': 'API Key inválida, por favor informe uma API Key válida.',
+    'auth/user-disabled': 'O usuário correspondente à credencial fornecida foi desativado.',
+    'auth/user-not-found': 'O usuário não correponde à nenhuma credencial.',
+    'auth/weak-password': 'A senha é muito fraca.',
+    'auth/email-already-in-use': 'Já existe uma conta com o endereço de email fornecido.',
+    'auth/invalid-email': 'O endereço de e-mail não é válido.',
+    'auth/account-exists-with-different-credential': 'E-mail já associado a outra conta.',
+    'auth/credential-already-in-use': 'Já existe uma conta para esta credencial.',
+    'auth/timeout': 'Excedido o tempo de resposta. O domínio pode não estar autorizado para realizar operações.',
+    'auth/invalid-credential': 'Email ou senha incorretos.',
+    'auth/wrong-password': 'Senha incorreta.',
+    'auth/missing-password': 'Por favor informe uma senha.',
+    'auth/network-request-failed': 'Falha de conexão com a rede.',
+    'auth/requires-recent-login': 'O último horário de acesso do usuário não atende ao limite de segurança.',
+    'auth/too-many-requests': 'As solicitações foram bloqueadas devido a atividades incomuns. Tente novamente depois que algum tempo.',
+    'auth/invalid-email-verified': 'O e-mail é inválido.',
+    'auth/invalid-password': 'A senha é inválida, precisa ter pelo menos 6 caracteres.',
+    
+  }
