e573b83f92428b7266bda05c7b25060648c1eef2
Moita <marcos.sales97@gmail.com>
2024-05-09 14:54:34 -0300

    feat: finish styling message input

--- src/components/Main/Chat/MessageInput/MessageInput.tsx
+++ src/components/Main/Chat/MessageInput/MessageInput.tsx
@@ -1,11 +1,12 @@
+import styles from './MessageInput.module.scss'
 import { Input } from '@/components'
 import { BiImage } from 'react-icons/bi'
 import { MdAttachFile } from 'react-icons/md'
 
 const MessageInput = () => {
   return (
-    <div>
-      <Input name="message" placeholder="Digite sua mensagem"  />
+    <div className={styles.wrapper}>
+      <Input name="message" placeholder="Digite sua mensagem" className={styles.messageInput} />
       <i><MdAttachFile /></i>
       <i><BiImage /></i>
     </div>
